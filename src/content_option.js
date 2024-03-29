4f46a894e6eff8a3dd9caa1b9f2609cf64aef762
Ali <Alipanahi090@gmail.com>
2024-03-29 15:54:07 +0100

    portfoli v1

---
+++ src/content_option.js
@@ -0,0 +1,122 @@
+const logotext = "Ali";
+const meta = {
+    title: "Ali Panahi",
+    description: "I’m Ali Panahi , a passionate UX/UI Desginer and Developer.",
+};
+
+const introdata = {
+    title: "I’m Ali Panahi",
+    animated: {
+        first: "I love Designing and codding",
+        second: "Merging creativity with functionality ",
+        third: "through the lens of Human-Centered Design.",
+    },
+    description: "It's not just about making things look good; it's about creating experiences that resonate with users on a deeper level.",
+    your_img_url: "https://26459559.fs1.hubspotusercontent-eu1.net/hub/26459559/hubfs/IMG_9775.jpeg?width=2080&height=3120&name=IMG_9775.jpeg",
+};
+
+const dataabout = {
+    title: "Get to know Me...👨🏻‍💻",
+    aboutme: "Hi, I'm Ali. My life is a mix of different passions and big change s. I grew up in Iran, where I loved playing football and was studying civil engineering. Football taught me to work with others and to keep going, even when it's tough. Civil engineering taught me how to solve problems. But deep inside, I was always excited about something more creative and tech-related. This interest became clear when I got injured in football and realized during my civil engineering internship that I really liked the digital world more than building things. So, I decided to study design and programming at Fontys University in Eindhoven. This was a big change for me, moving from Iran to a new country and a new field of study. At Fontys, I started learning the basics of design and programming. I used tools like Figma and Adobe and learned web languages like HTML, CSS, and JavaScript. I did a project where I made a website for a client. This project helped me use what I learned in a real way. I also learned a lot about myself at Fontys. I learned how to work alone and with others, to think creatively, and to enjoy challenges. I chose to focus on making mobile apps and learned about branding and marketing too. In my internship at a company called Edorado, I used my design skills in real projects. This experience taught me a lot.",
+};
+const worktimeline = [{
+        jobtitle: "UX/UI Designer",
+        where: "Edorado - Internship",
+        date: "Sep 2023",
+    },
+    {
+        jobtitle: "Loyalty of Lucifer",
+        where: "Fontys UAC - Semester 3",
+       date: "Sep 2022",
+    },
+    {
+       jobtitle: "Smart Mobile",
+      where: "Fontys UAC - Semester 4",
+      date: "May 2023",
+    },
+];
+
+const skills = [{
+        name: "Figma",
+        value: 100,
+    },
+    {
+        name: "Adobe Cloud",
+        value: 100,
+    },
+    {
+        name: "Javascript",
+        value: 1000,
+    },
+    {
+        name: "HTML & CSS",
+        value: 100,
+    },
+
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [,
+    {
+        img: "https://f.hubspotusercontent-eu1.net/hubfs/26459559/9171e8f87713026dc961a2b15861013fa5b2a05e.jpg",
+        description: "How Lucifer Coffee Brewed a Fresh Digital Strategyisdom of life consists in the elimination of non-essentials.",
+        link: "https://www.alipanahi.com/loyalties-of-lucifer",
+    },
+    {
+        img: "https://26459559.fs1.hubspotusercontent-eu1.net/hubfs/26459559/image%20135.png",
+        description: "Electric Boats, Enhanced Experiences: A UI/UX Design Internship Journey at Edorado",
+        link: "https://www.alipanahi.com/internship",
+    },
+    {
+        img: "https://www.alipanahi.com/hubfs/Screenshot%202023-04-03%20at%2015.07.45.png",
+        description: "A Student's Journey in iOS, Android, and PWA Development.",
+        link: "https://www.alipanahi.com/smart-mobile-apps",
+    },
+
+
+    
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "Alipanahi090@gmail.com",
+    YOUR_FONE: "0657311381",
+    description:  "Drop me a message!",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_0nvelqi",
+    YOUR_TEMPLATE_ID: "template_6qewvsg",
+    YOUR_USER_ID: "uTKDcPglFXBWCE8WK",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/in/alipanahi090/",
+    twitter: "https://twitter.com",
+    myCv: "https://drive.google.com/file/d/1uz_ayCB1P4S9V-XTXcWrdncBNs-Xolsi/view?usp=sharing",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
